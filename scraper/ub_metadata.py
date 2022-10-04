59526413dc2041ab12a61243fd9a19093ab66c22
KJ.O <nomeansno@Live.ca>
2022-10-04 19:43:24 +0900

    Initial commit

---
+++ scraper/ub_metadata.py
@@ -0,0 +1,9 @@
+DRIVER_PATH = '/Users/audaefi/chromedriver/chromedriver'
+#DRIVER_PATH = '/usr/bin/chromedriver'
+
+IMG_SRC_CSS = "figure.product-image > img"
+HREF_CSS = ".img-detail.goos-tag-product"
+PRODUCT_TITLE_CSS = ".product-title"
+PRICE_CSS = ".h3.product-price"
+SELLER_CSS = ".product-store"
+#NEXT_ARROW_BTN_CSS = f'.l_search_content > div > nav > ul > li:nth-child(1) > a'
